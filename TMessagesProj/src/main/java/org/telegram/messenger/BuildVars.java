092a6ddcbd1d95a6789c07cc1881cca6442b2afe
Laky64 <35636667+Laky-64@users.noreply.github.com>
2022-02-19 21:10:44 +0100

    Update to 1.6.1

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -21,7 +21,7 @@ public class BuildVars {
     public static boolean CHECK_UPDATES = false;
     public static boolean IGNORE_VERSION_CHECK = false;
     public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
-    public static int BUILD_VERSION = 2569;
+    public static int BUILD_VERSION = 2570;
     public static String BUILD_VERSION_STRING = "1.6.1";
     public static int TELEGRAM_BUILD_VERSION = 2566;
     public static String TELEGRAM_VERSION_STRING = "8.5.4";
