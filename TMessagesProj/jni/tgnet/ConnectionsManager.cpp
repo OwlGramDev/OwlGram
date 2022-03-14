682fed2c18f352c0210fa0534216b8d11264c1f8
Laky-64 <iraci.matteo@gmail.com>
2022-03-14 18:29:08 +0100

    Revert & Fix C++ crash

--- TMessagesProj/jni/tgnet/ConnectionsManager.cpp
+++ TMessagesProj/jni/tgnet/ConnectionsManager.cpp
@@ -135,8 +135,39 @@ ConnectionsManager::~ConnectionsManager() {
 }
 
 ConnectionsManager& ConnectionsManager::getInstance(int32_t instanceNum) {
-    static ConnectionsManager instance(instanceNum);
-    return instance;
+    switch (instanceNum) {
+        case 0:
+            static ConnectionsManager instance0(0);
+            return instance0;
+        case 1:
+            static ConnectionsManager instance1(1);
+            return instance1;
+        case 2:
+            static ConnectionsManager instance2(2);
+            return instance2;
+        case 3:
+            static ConnectionsManager instance3(3);
+            return instance3;
+        case 4:
+            static ConnectionsManager instance4(4);
+            return instance4;
+        case 5:
+            static ConnectionsManager instance5(5);
+            return instance5;
+        case 6:
+            static ConnectionsManager instance6(6);
+            return instance6;
+        case 7:
+            static ConnectionsManager instance7(7);
+            return instance7;
+        case 8:
+            static ConnectionsManager instance8(8);
+            return instance8;
+        case 9:
+        default:
+            static ConnectionsManager instance9(9);
+            return instance9;
+    }
 }
 
 int ConnectionsManager::callEvents(int64_t now) {
