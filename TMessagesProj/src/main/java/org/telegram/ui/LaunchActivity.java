16f5a99c99ecaacfc2b8f2d079fd3fb7d229369f
Laky64 <35636667+Laky-64@users.noreply.github.com>
2022-05-18 17:47:55 +0200

    Added Passport & User id resolver

--- TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/LaunchActivity.java
@@ -1341,6 +1341,7 @@ public class LaunchActivity extends BasePermissionsActivity implements ActionBar
         boolean pushOpened = false;
         long push_user_id = 0;
         long push_chat_id = 0;
+        long profile_user_id = 0;
         int push_enc_id = 0;
         int push_msg_id = 0;
         int open_settings = 0;
@@ -1872,8 +1873,11 @@ public class LaunchActivity extends BasePermissionsActivity implements ActionBar
                                     if (url.startsWith("tg:resolve") || url.startsWith("tg://resolve")) {
                                         url = url.replace("tg:resolve", "tg://telegram.org").replace("tg://resolve", "tg://telegram.org");
                                         data = Uri.parse(url);
+                                        String number = data.getQueryParameter("phone");
                                         username = data.getQueryParameter("domain");
-                                        if ("telegrampassport".equals(username)) {
+                                        if (number != null) {
+                                            username = number;
+                                        } else if ("telegrampassport".equals(username)) {
                                             username = null;
                                             auth = new HashMap<>();
                                             String scope = data.getQueryParameter("scope");
@@ -2050,6 +2054,16 @@ public class LaunchActivity extends BasePermissionsActivity implements ActionBar
                                         int intCode = Utilities.parseInt(data.getQueryParameter("code"));
                                         if (intCode != 0) {
                                             code = "" + intCode;
+                                        }
+                                    } else if (url.startsWith("tg:user") || url.startsWith("tg://user")) {
+                                        url = url.replace("tg:user", "tg://telegram.org").replace("tg://user", "tg://telegram.org");
+                                        data = Uri.parse(url);
+                                        String userID = data.getQueryParameter("id");
+                                        if (userID != null) {
+                                            try {
+                                                profile_user_id = Long.parseLong(userID);
+                                            } catch (NumberFormatException ignore) {
+                                            }
                                         }
                                     } else if (url.startsWith("tg:openmessage") || url.startsWith("tg://openmessage")) {
                                         url = url.replace("tg:openmessage", "tg://telegram.org").replace("tg://openmessage", "tg://telegram.org");
@@ -2421,6 +2435,18 @@ public class LaunchActivity extends BasePermissionsActivity implements ActionBar
                 if (actionBarLayout.presentFragment(fragment, false, true, true, false)) {
                     pushOpened = true;
                     drawerLayoutContainer.closeDrawer();
+                }
+            } else if (profile_user_id != 0) {
+                Bundle args = new Bundle();
+                args.putLong("user_id", profile_user_id);
+                ProfileActivity fragment = new ProfileActivity(args);
+                AndroidUtilities.runOnUIThread(() -> presentFragment(fragment, false, false));
+                if (AndroidUtilities.isTablet()) {
+                    actionBarLayout.showLastFragment();
+                    rightActionBarLayout.showLastFragment();
+                    drawerLayoutContainer.setAllowOpenDrawer(false, false);
+                } else {
+                    drawerLayoutContainer.setAllowOpenDrawer(true, false);
                 }
             } else if (showDialogsList) {
                 if (!AndroidUtilities.isTablet()) {
