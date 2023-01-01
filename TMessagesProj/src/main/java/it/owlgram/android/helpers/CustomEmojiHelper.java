f0587607cb610f14ce2d80ff655fa04121f2686d
Laky64 <iraci.matteo@gmail.com>
2023-01-01 16:20:08 +0100

    Added Custom Emoji Loader

--- TMessagesProj/src/main/java/it/owlgram/android/helpers/CustomEmojiHelper.java
+++ TMessagesProj/src/main/java/it/owlgram/android/helpers/CustomEmojiHelper.java
@@ -1,19 +1,37 @@
 package it.owlgram.android.helpers;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.os.SystemClock;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.telegram.messenger.AndroidUtilities;
+import org.telegram.messenger.ApplicationLoader;
 import org.telegram.messenger.BuildVars;
 import org.telegram.messenger.FileLog;
+import org.telegram.messenger.NotificationCenter;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+import it.owlgram.android.OwlConfig;
 
 public class CustomEmojiHelper {
     private static Typeface systemEmojiTypeface;
     private static boolean loadSystemEmojiFailed = false;
     private static final String EMOJI_FONT_AOSP = "NotoColorEmoji.ttf";
+    private static final SharedPreferences preferences = ApplicationLoader.applicationContext.getSharedPreferences("EmojiPackConfig", Context.MODE_PRIVATE);
+    private static boolean loadingPack = false;
+    private static final ArrayList<EmojiPackInfo> emojiPacksInfo = new ArrayList<>();
 
     public static Typeface getSystemEmojiTypeface() {
         if (!loadSystemEmojiFailed && systemEmojiTypeface == null) {
@@ -48,5 +66,111 @@ public class CustomEmojiHelper {
             }
         }
         return systemEmojiTypeface;
+    }
+
+    public static String getSelectedPackName() {
+        return emojiPacksInfo
+                .stream()
+                .map(EmojiPackInfo::getPackName)
+                .filter(emojiPackInfo -> Objects.equals(emojiPackInfo, OwlConfig.emojiPackSelected))
+                .findFirst()
+                .orElse("Apple");
+    }
+
+    public static boolean loadedPackInfo() {
+        return !emojiPacksInfo.isEmpty();
+    }
+
+    public static void loadEmojisInfo() {
+        if (loadingPack) {
+            return;
+        }
+        loadingPack = true;
+        new Thread() {
+            @Override
+            public void run() {
+                try {
+                    String json = preferences.getString("emoji_packs", null);
+                    if (json == null || System.currentTimeMillis() - getLastUpdate() > 10 * 60 * 1000) {
+                        json = new StandardHTTPRequest("https://app.owlgram.org/emoji_packs").request();
+                        preferences.edit().putString("emoji_packs", json).apply();
+                        updateLastUpdate();
+                    }
+                    emojiPacksInfo.addAll(loadFromJson(json));
+                } catch (Exception ignored) {
+                    SystemClock.sleep(1000);
+                } finally {
+                    loadingPack = false;
+                    AndroidUtilities.runOnUIThread(() -> NotificationCenter.getGlobalInstance().postNotificationName(NotificationCenter.emojiLoaded));
+                }
+            }
+        }.start();
+    }
+
+    private static Long getLastUpdate() {
+        return preferences.getLong("last_update", 0);
+    }
+
+    private static void updateLastUpdate() {
+        preferences.edit().putLong("emoji_packs_last_update", System.currentTimeMillis()).apply();
+    }
+
+    private static ArrayList<EmojiPackInfo> loadFromJson(String json) throws JSONException {
+        ArrayList<EmojiPackInfo> packs = new ArrayList<>();
+        JSONArray array = new JSONArray(json);
+        for (int i = 0; i < array.length(); i++) {
+            JSONObject obj = array.getJSONObject(i);
+            packs.add(new EmojiPackInfo(
+                    obj.getString("name"),
+                    obj.getString("file"),
+                    obj.getString("preview"),
+                    obj.getString("id"),
+                    obj.getLong("file_size")
+            ));
+        }
+        return packs.stream()
+                .sorted(Comparator.comparing(EmojiPackInfo::getPackName))
+                .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public interface EmojiPacksInfoLoadedListener {
+        void onEmojiPackInfoLoaded(ArrayList<EmojiPackInfo> emojiPackInfos);
+        void onEmojiPackInfoLoadFailed();
+    }
+
+    public static class EmojiPackInfo {
+        private final String packName;
+        private final String fileLink;
+        private final String previewLink;
+        private final String packId;
+        private final Long packSize;
+
+        public EmojiPackInfo(String packName, String fileLink, String previewLink, String packId, Long packSize) {
+            this.packName = packName;
+            this.fileLink = fileLink;
+            this.previewLink = previewLink;
+            this.packId = Objects.equals(packId, "apple") ? "default" : packId;
+            this.packSize = packSize;
+        }
+
+        public String getPackName() {
+            return packName;
+        }
+
+        public String getFileLink() {
+            return fileLink;
+        }
+
+        public String getPreviewLink() {
+            return previewLink;
+        }
+
+        public String getPackId() {
+            return packId;
+        }
+
+        public Long getPackSize() {
+            return packSize;
+        }
     }
 }
