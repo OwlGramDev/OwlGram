4de4fb149a53dec67b0d2a219ea3421b05788c5c
Laky64 <iraci.matteo@gmail.com>
2023-01-05 14:22:03 +0100

    Made more generic the EmojiSetBulletin

--- TMessagesProj/src/main/java/it/owlgram/android/components/EmojiSetBulletinLayout.java
+++ TMessagesProj/src/main/java/it/owlgram/android/components/EmojiSetBulletinLayout.java
@@ -7,17 +7,17 @@ import androidx.annotation.NonNull;
 
 import org.telegram.messenger.LocaleController;
 import org.telegram.messenger.R;
+import org.telegram.ui.ActionBar.Theme;
 import org.telegram.ui.Components.Bulletin;
 
 import it.owlgram.android.helpers.CustomEmojiHelper;
 
 @SuppressLint("ViewConstructor")
 public class EmojiSetBulletinLayout extends Bulletin.TwoLineLayout {
-
-    public EmojiSetBulletinLayout(@NonNull Context context, CustomEmojiHelper.EmojiPackBase data) {
-        super(context, null);
-        titleTextView.setText(LocaleController.getString("EmojiSetRemoved", R.string.EmojiSetRemoved));
-        subtitleTextView.setText(LocaleController.formatString("EmojiSetRemovedInfo", R.string.EmojiSetRemovedInfo, data.getPackName()));
+    public EmojiSetBulletinLayout(@NonNull Context context, String title, String description, CustomEmojiHelper.EmojiPackBase data, Theme.ResourcesProvider resourcesProvider) {
+        super(context, resourcesProvider);
+        titleTextView.setText(title);
+        subtitleTextView.setText(description);
         imageView.setImage(data.getPreview(), null, null);
     }
 }
